d45cc62ef7803e75d9de5c226aecf08ba30b12ad
Acexy <thankjava@foxmail.com>
2018-01-05 11:48:03 +0800

    新增md5命令

---
+++ plugins/md5.js
@@ -0,0 +1,17 @@
+/**
+ * @Author:acexy@thankjava.com
+ * 2018/1/5
+ * @Description:
+ */
+const commandBuilder = require('../lib/commandBuilder');
+const program = new commandBuilder();
+
+const md5 = require('md5');
+
+program.command("md5 <content>", "将输入的数据进行md5加密 例如: md5 12345678", async function (content) {
+    return md5(content);
+});
+
+module.exports.exec = async reqData => {
+    return await program.exec(reqData.req.splitContent);
+};
