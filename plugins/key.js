0e29a1cf91479ce0aca786234f95637eb5e995ee
Acexy <thankjava@foxmail.com>
2018-01-09 21:41:58 +0800

    新增key命令

--- plugins/key.js
+++ plugins/key.js
@@ -12,9 +12,6 @@ const program = new commandBuilder();
 const SQL = global.config.sql;
 
 program.command("key set <key> <value>", "创建/更新关键字检索信息 例如: key set jquery jQuery是一个快速、简洁的JavaScript框架", async function (key, value, openId) {
-    if (!key || !value) {
-        return "参数key或者value无效";
-    }
     var response = await mysqlPool.exec(SQL.keySetCount, [key, openId]);
     if (response.flag) {
         if (response.data[0].count == 0) {
@@ -25,14 +22,28 @@ program.command("key set <key> <value>", "创建/更新关键字检索信息 例
             }
         } else {
             // 历史已经创建
+            response = await mysqlPool.exec(SQL.keySetUpdate, [value, key, openId]);
+            if (response.flag) {
+                return "关键字 " + key + " 更新完成, 可通过 key find " + key + " 查询";
+            }
         }
     }
-    return "";
+    return "关键字 " + key + " 处理失败";
 });
 /**
  * 模糊查询自己创建的关键字信息
  */
-program.command("key find <content>", "模糊查询自己创建的关键字信息 例如: key find 帐号", async function (content) {
+program.command("key find <key>", "模糊查询自己创建的关键字信息 例如: key find 帐号", async function (key, openId) {
+    var response = await mysqlPool.exec(SQL.keySetFindKey, [openId, key]);
+    if (response.flag) {
+        var list = response.data;
+        var content = '';
+        for (var index in list) {
+            content += (index + 1) + '. ' + list[index].keyword + "\t" + list[index].information + "\n";
+        }
+        return content;
+    }
+    return "查询内容失败"
 });
 
 module.exports.exec = async reqData => {
