6423b85bef61a24ed6edd4b912f0fbfcfa1d1ca1
Acexy <thankjava@foxmail.com>
2018-05-03 09:11:05 +0800

    调整命令

--- plugins/url.js
+++ plugins/url.js
@@ -5,11 +5,11 @@
 const commandBuilder = require('../lib/commandBuilder');
 const program = new commandBuilder();
 
-program.command("url en <content>", "将输入的数据进行URLEncode 例如: url en \"你好Acexy开发者工具\"", async function (content) {
+program.command("url encode <content>", "将输入的数据进行URLEncode 例如: url encode \"你好Acexy开发者工具\"", async function (content) {
     return encodeURI(content);
 });
 
-program.command("url de <content>", "将输入的数据进行URLDecode 例如: url de %e4%bd%a0%e5%a5%bdAcexy%e5%bc%80%e5%8f%91%e8%80%85%e5%b7%a5%e5%85%b7", async function (content) {
+program.command("url decode <content>", "将输入的数据进行URLDecode 例如: url decode %e4%bd%a0%e5%a5%bdAcexy%e5%bc%80%e5%8f%91%e8%80%85%e5%b7%a5%e5%85%b7", async function (content) {
     return decodeURI(content);
 });
 module.exports.exec = async reqData => {
