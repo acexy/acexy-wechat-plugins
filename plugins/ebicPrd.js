a9a1f8bf32808c7e9d107bbba82315eb2361aa40
Acexy <acexy@thankjava.com>
2020-05-20 18:06:30 +0800

    新增命令

--- plugins/ebicPrd.js
+++ plugins/ebicPrd.js
@@ -14,7 +14,7 @@ const xmlJson = require('../../utils/lib/xmlJson');
 const cmdEbic = global.config.cmdEbic;
 
 program.version("1.0.0");
-program.command("ebic-prd bindSubAppid <env> <subMchId> <subAppId>", '为子商户号绑定subAppId \n 例如: ebic-prd bindSubAppid online 3333333 wxwxwxwxx' +
+program.command("ebicPrd bindSubAppid <env> <subMchId> <subAppId>", '为子商户号绑定subAppId \n 例如: ebicPrd bindSubAppid online 3333333 wxwxwxwxx' +
     ' \n 参数说明 \n\t env[执行环境]: online = 线上环境; offline = 线下环境', async function (env, subMchId, subAppId) {
 
         if (env != 'prd-online' && env != 'prd-offline') {
@@ -43,7 +43,7 @@ program.command("ebic-prd bindSubAppid <env> <subMchId> <subAppId>", '为子商�
     return await doRequest(wxpay, reqData);
 });
 
-program.command("ebic-prd addPayUrl <env> <subMchId> <payUrl>", '为子商户号添加支付目录 \n 例如: ebic-prd bindSubAppid online 3333333 https://pay.com/' +
+program.command("ebiPrd addPayUrl <env> <subMchId> <payUrl>", '为子商户号添加支付目录 \n 例如: ebicPrd bindSubAppid online 3333333 https://pay.com/' +
     ' \n 参数说明 \n\t env[执行环境]: test = 测试环境', async function (env, subMchId, payUrl) {
 
     if (env != 'test' && env != 'prd-online' && env != 'prd-offline') {
