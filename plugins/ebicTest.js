6fbaac6ffca8e136cae69c0600ca152b1bb32861
Acexy <acexy@thankjava.com>
2020-07-02 15:25:34 +0800

    命令调整

--- plugins/ebicTest.js
+++ plugins/ebicTest.js
@@ -14,14 +14,9 @@ const xmlJson = require('../../utils/lib/xmlJson');
 const cmdEbic = global.config.cmdEbic;
 
 program.version("1.0.0");
-program.command("ebic bindSubAppid <env> <subMchId> <subAppId>", '为子商户号绑定subAppId \n 例如: ebic bindSubAppid test 3333333 wxwxwxwxx' +
-    ' \n 参数说明 \n\t env[执行环境]: test = 测试环境', async function (env, subMchId, subAppId) {
+program.command("ebicTest bindSubAppid <subMchId> <subAppId>", '为子商户号绑定subAppId \n 例如: ebicTest bindSubAppid 3333333 wxwxwxwxx', async function (subMchId, subAppId) {
 
-    if (env != 'test') {
-        return "env 参数指定了无效值";
-    }
-
-    let config = cmdEbic[env];
+    let config = cmdEbic['test'];
     if (!config) {
         return "指定的环境未有相应的配置";
     }
@@ -43,14 +38,9 @@ program.command("ebic bindSubAppid <env> <subMchId> <subAppId>", '为子商户�
     return await doRequest(wxpay, reqData);
 });
 
-program.command("ebic addPayUrl <env> <subMchId> <payUrl>", '为子商户号添加支付目录 \n 例如: ebic addPayUrl test 3333333 https://pay.com/' +
-    ' \n 参数说明 \n\t env[执行环境]: test = 测试环境', async function (env, subMchId, payUrl) {
+program.command("ebicTest addPayUrl <env> <subMchId> <payUrl>", '为子商户号添加支付目录 \n 例如: ebicTest addPayUrl 3333333 https://pay.com/', async function (subMchId, payUrl) {
 
-    if (env != 'test' && env != 'prd-online' && env != 'prd-offline') {
-        return "env 参数指定了无效值";
-    }
-
-    let config = cmdEbic[env];
+    let config = cmdEbic["test"];
     if (!config) {
         return "指定的环境未有相应的配置";
     }
@@ -75,7 +65,7 @@ program.command("ebic addPayUrl <env> <subMchId> <payUrl>", '为子商户号添�
 const doRequest = async (wxpay, reqData) => {
     let response = await wxpay.requestWithCert(WXPaySDK.WXPayConstants.DOMAIN + '/secapi/mch/addsubdevconfig', reqData);
     response = await xmlJson.xml2Json(response);
-    if (response.xml.return_code == 'SUCCESS' && response.xml.result_code == 'SUCCESS') {
+    if (response.xml.return_code === 'SUCCESS' && response.xml.result_code === 'SUCCESS') {
         return '操作成功';
     } else {
         return '操作失败: ' + !response.xml.err_code_des ? response.xml.return_msg : response.xml.err_code_des;
