b732fd6fd757982073322a9c55ceff1b069e3c18
Acexy <acexy@thankjava.com>
2020-07-13 15:36:21 +0800

    bugfix :  ebicTest 返回微信错误信息异常

--- plugins/ebicTest.js
+++ plugins/ebicTest.js
@@ -10,7 +10,6 @@ const fs = require('fs');
 const WXPaySDK = require('../../utils/lib/wxsdk');
 
 const xmlJson = require('../../utils/lib/xmlJson');
-const logger = require('../../utils/lib/logger');
 
 
 const cmdEbic = global.config.cmdEbic;
@@ -66,12 +65,12 @@ program.command("ebicTest addPayUrl <subMchId> <payUrl>", '为子商户号添加
 
 const doRequest = async (wxpay, reqData) => {
     let response = await wxpay.requestWithCert(WXPaySDK.WXPayConstants.DOMAIN + '/secapi/mch/addsubdevconfig', reqData);
-    logger.info('请求微信:' + JSON.stringify(reqData) + ' 微信相应: ' + response);
     response = await xmlJson.xml2Json(response);
     if (response.xml.return_code === 'SUCCESS' && response.xml.result_code === 'SUCCESS') {
         return '操作成功';
     } else {
-        return '操作失败: ' + !response.xml.err_code_des ? response.xml.return_msg : response.xml.err_code_des;
+        return '操作失败: ' + (!response.xml.err_code_des ? response.xml.return_msg : response.xml.err_code_des);
+
     }
 }
 
