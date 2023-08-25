3caeda8c12539371008e6f38ecee713590103f5b
Acexy <thankjava@gmail.com>
2023-08-25 15:27:03 +0800

    调整gcode默认过期时间

--- plugins/gcode.js
+++ plugins/gcode.js
@@ -65,7 +65,7 @@ program.command("gcode get <secretNo>", "获取Google算法产生的验证码", 
     if (!response.data[0] || !response.data[0].secret_code) {
         return '无效的令牌编号 \n可通过 gcode list 查询已配置Google令牌'
     }
-    return totp(response.data[0].secret_code);
+    return totp(response.data[0].secret_code, {period: 30});
 });
 
 
