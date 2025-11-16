6c00396ff814bedff095f8d6f9c803db2ec86a17
Acexy <acexy@acexy.cn>
2025-11-16 14:21:47 +0800

    新增tx子命令

--- plugins/tx.js
+++ plugins/tx.js
@@ -20,8 +20,8 @@ program.command("tx i <fs> <ts> <fm> <tm> <v>", "添加交易记录 例如: tx i
 
 // 添加交易记录
 program.command("tx d <s>", "交易详情统计 例如: tx d b", async function (s) {
-    let S =s.toUpperCase()
-    let response = await mysqlPool.exec(SQL.txDetail, [S,S,S,S,S,S]);
+    let S = s.toUpperCase()
+    let response = await mysqlPool.exec(SQL.txDetail, [S, S, S, S, S, S]);
     if (response.flag) {
         if (response.data && response.data.length > 0) {
             let resp = '';
@@ -33,6 +33,38 @@ program.command("tx d <s>", "交易详情统计 例如: tx d b", async function 
         return "暂无记录";
     }
     return "查询异常";
+});
+
+// 添加交易记录
+program.command("tx l <s>", "交易记录列表 例如: tx l b", async function (s) {
+    let S = s.toUpperCase()
+    let response = await mysqlPool.exec(SQL.txList, [S, S, S]);
+    if (response.flag) {
+        if (response.data && response.data.length > 0) {
+            let resp = '创建时间    方向  价值  数量  均价\n';
+            for (let i = 0; i < response.data.length; i++) {
+                let row = response.data[i];
+                let str = '';
+                for (let k in response.fields) {
+                    str += row[response.fields[k].name] + ',';
+                }
+                str = str.substring(0, str.length - 1)
+                resp += str + '\n';
+            }
+            return resp;
+        }
+        return "暂无记录";
+    }
+    return "查询异常";
+});
+
+program.command("tx c <s>", "关闭交易 例如: tx c b", async function (s) {
+    let S = s.toUpperCase()
+    let response = await mysqlPool.exec(SQL.txClose, [S, S]);
+    if (response.flag) {
+        return "处理成功";
+    }
+    return "处理失败";
 });
 
 module.exports.exec = async reqData => {
